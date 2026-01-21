2db4820f538af6f3b651ae7dd61c63edb194d953
Mark <palokid@gmail.com>
2026-01-21 16:02:58 -0300

    💻🐧 Updates

--- js/global/sidecar.js
+++ js/global/sidecar.js
@@ -36,48 +36,15 @@ const SIDECAR = {
 
   // toggle expand buttons
   toggleSidecarExpands: function() {
-    const activeClass = 'active';
-    const activeModifier = '--active';
-    const expandClass = 'menu__expand';
+    var btnExpand = document.querySelectorAll('#sidecar-nav .btn-expand');
 
-    const expands = document.querySelectorAll(`.mobile-nav .${expandClass}`);
-
-    function collapseMenu(expand, panel) {
-      expand.classList.remove(`${expandClass}${activeModifier}`);
-      panel.style.height = panel.offsetHeight + 'px';
-      setTimeout(() => { panel.style.height = '0px' }, 0);
-      let transitionDuration = getComputedStyle(document.documentElement).getPropertyValue('--transition-duration-m').replace('s', '');
-      transitionDuration = parseFloat(transitionDuration) * 1000;
-      setTimeout(() => {
-        panel.classList.remove(`${activeClass}`);  
-      }, transitionDuration);
-    }
-
-    function expandMenu(expand, panel) {
-      expand.classList.add(`${expandClass}${activeModifier}`);
-      panel.classList.add(`${activeClass}`);
-      panel.style.height = 'auto';
-      let panelHeight = panel.offsetHeight + 'px';
-      panel.style.height = '0px';
-      setTimeout(() => { panel.style.height = panelHeight }, 0);
-      let transitionDuration = getComputedStyle(document.documentElement).getPropertyValue('--transition-duration-m').replace('s', '');
-      transitionDuration = parseFloat(transitionDuration) * 1000;
-      setTimeout(() => { panel.style.height = 'auto' }, transitionDuration);
-    }
-
-    function onClick() {
-      const expand = this;
-      const panel = expand.parentNode.querySelector('ul');
-      (expand.classList.contains(`${expandClass}${activeModifier}`))
-        ? collapseMenu(expand, panel)
-        : expandMenu(expand, panel);
-    }
-
-    if (expands.length) {
-      expands.forEach(function (expand) {
-        expand.addEventListener('click', onClick);
+    btnExpand.forEach(function(btn) {
+      btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        // toggle active class on sibling element
+        this.parentElement.classList.toggle('active');
       });
-    }
+    });
   }
 }
 
