b862c39702f2477bcc5098741b5f8835b2855198
Mark <palokid@gmail.com>
2026-01-22 17:30:39 -0300

    💻🐧 Updates

--- js/global/helper.js
+++ js/global/helper.js
@@ -1,9 +1,12 @@
+import { CountUp } from 'countup.js';
+
 const HELPER = {
    init: function () {
       this.lazyIMGs();
       this.embedResponsively();
       this.promoArea();
       this.ppQuote();
+      this.ppPromoAreaStats();
    },
    lazyIMGs: function () {
 
@@ -260,6 +263,50 @@ const HELPER = {
          }
 
       );
+   },
+
+   ppPromoAreaStats: function() {
+
+      // stats section
+      const stats = document.querySelectorAll('.stat-number');
+
+      const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+         const el = entry.target;
+         const countUp = el._countUp;
+
+         if (entry.isIntersecting) {
+            if (!countUp.error && !countUp.isRunning) {
+            countUp.start();
+            }
+         } else {
+            // 👇 reset when out of view
+            countUp.reset();
+         }
+      });
+      }, {
+      rootMargin: '0px 0px 0px 0px',
+      threshold: 0
+      });
+
+      stats.forEach(el => {
+      const start = parseFloat(el.dataset.statStart) || 0;
+      const end = parseFloat(el.dataset.statEnd) || 0;
+      const decimals = (end.toString().split('.')[1] || '').length;
+
+      el._countUp = new CountUp(el, end, {
+         startVal: start,
+         decimalPlaces: decimals,
+         duration: 3,
+         useGrouping: true,
+      });
+
+      observer.observe(el);
+      });
+
+      // bg 
+
+      
    }
 
 }
